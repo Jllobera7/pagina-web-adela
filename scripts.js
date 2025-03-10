3b94760560dce60507b823daa19b99600e1d8e8d
Juan Cristian Llobera Vázquez <jlloberavazquez@cifpfbmoll.eu>
2025-03-10 14:31:29 +0100

    version definitiva de mi parte del trabajo

--- scripts.js
+++ scripts.js
@@ -1,4 +1,87 @@
-/* -----------------------------JUAN-----------------------------------------------*/
+/* ======================================================================================*/
+/* ==================================JUAN================================================*/
+/* ======================================================================================*/
+
+/*-Ejercicio 5-------------------------------------------------------------------------------
+ He capturado el h1 del texto "Gipsy Arome" del header en una variable y he añadido un evento 
+ al pasar el ratón por encima para cambiar el color y aumentar ligeramente el tamaño.
+ Al quitar el ratón, el color y tamaño vuelven a la normalidad.
+---------------------------------------------------------------------------------------------*/
+
+
+let titulo = document.getElementById('gypsy'); // Captura el h1 del header
+
+// Evento al pasar el mouse
+titulo.addEventListener('mouseover', function() {
+    titulo.style.color ='#285ae694'; // Tono ámbar cálido
+    titulo.style.transform = 'scale(1.03)'; // Ligerísimo aumento
+    titulo.style.transition = 'all 0.4s'; 
+
+});
+
+// Evento al quitar el mouse
+titulo.addEventListener('mouseout', function() {
+    titulo.style.color = ''; // Restaura el color por defecto del h1
+    titulo.style.transform = ''; // Restaura la escala por defecto
+});
+
+
+/*-----------------------------BONUS EXTRA-----------------------------------------
+-------------PULVERIZACION VSUAL Y SONORA EN HEADER AL HACER CLICK----------------*/ 
+
+
+document.getElementById('header').addEventListener('click', function(event) {  /*Aqui creo el evento al hacer click en el header*/
+    
+    let sonido = new Audio(`${BASE_PATH}Audios/spray_perfume.mp3`);
+    sonido.play();
+
+    // Crear más partículas con un radio más grande
+    for (let i = 0; i < 50; i++) { // Aumentamos la cantidad de partículas
+        crearParticula(event.clientX, event.clientY);
+    }
+});
+
+
+/* Creacion de particulas  */
+function crearParticula(x, y) {
+    let particula = document.createElement('span');
+    document.body.appendChild(particula);
+
+    // Tamaño aleatorio entre 4px y 19px
+    let size = Math.random() * 15 + 4;
+    particula.classList.add('particula');
+    particula.style.width = size + 'px';
+    particula.style.height = size + 'px';
+    particula.style.left = x + 'px';
+    particula.style.top = y + 'px';
+
+    // Movimiento aleatorio más grande
+    let angle = Math.random() * 2 * Math.PI; // Dirección aleatoria
+    let speed = Math.random() * 100 + 50; // Más dispersión
+    let translateX = Math.cos(angle) * speed;
+    let translateY = Math.sin(angle) * speed;
+
+    // Usar variables CSS para la animación
+    particula.style.setProperty('--translateX', `${translateX}px`);
+    particula.style.setProperty('--translateY', `${translateY}px`);
+
+    // Activar la animación
+    setTimeout(() => {
+        particula.classList.add('animar');
+    }, 10);
+
+    // Eliminar la partícula después de la animación
+    setTimeout(() => {
+        particula.remove();
+    }, 1000);
+}
+
+/*-Ejercicio 6-------------------------------------------------------------------------------
+ He creado un carrusel de imagenes de perfumes en el footer de la pagina principal 
+ con botones de navegación para avanzar y retroceder.
+ He añadido un evento a los botones para mover el carrusel en la dirección correspondiente.
+---------------------------------------------------------------------------------------------*/
+
 let indice = 0; // Índice actual del carrusel
 
 function moverCarrusel(direccion) {
@@ -49,6 +132,11 @@ window.addEventListener('resize', () => {
     moverCarrusel(0);
 });
 
+/*-Ejercicio 7-------------------------------------------------------------------------------
+ He creado una función para ampliar una imagen del carrusel al hacer clic en ella.
+ La imagen ampliada se reduce al hacer clic en otra imagen del carrusel o en ella misma.
+---------------------------------------------------------------------------------------------*/
+
 function ampliarImagen(img) {
     const todasLasImagenes = document.querySelectorAll('.carrusel img');
     todasLasImagenes.forEach(imagen => {
@@ -58,82 +146,6 @@ function ampliarImagen(img) {
     });
     img.classList.toggle('agrandada');
 }
-
-
-/* Ejercicio 5
- He capturado el h1 del texto "Gipsy Arome" del header en una variable y he añadido un evento 
- al pasar el ratón por encima para cambiar el color y aumentar ligeramente el tamaño.
-Al quitar el ratón, el color y tamaño vuelven a la normalidad.
-*/
-
-
-let titulo = document.getElementById('gypsy'); // Captura el h1 del header
-
-// Evento al pasar el mouse
-titulo.addEventListener('mouseover', function() {
-    titulo.style.color ='#285ae694'; // Tono ámbar cálido
-    titulo.style.transform = 'scale(1.03)'; // Ligerísimo aumento
-    titulo.style.transition = 'all 0.4s'; 
-
-});
-
-// Evento al quitar el mouse
-titulo.addEventListener('mouseout', function() {
-    titulo.style.color = ''; // Restaura el color por defecto del h1
-    titulo.style.transform = ''; // Restaura la escala por defecto
-});
-/*-------------------------------------------------------------------------------------------------*/ 
-document.getElementById('header').addEventListener('click', function(event) {
-    
-    // Reproducir sonido de pulverización
-    let sonido = new Audio('../Audios/spray_perfume.mp3'); // Sonido de spray
-    sonido.play();
-
-    let sonidos = new Audio('Audios/spray_perfume.mp3'); // Sonido de spray
-    sonidos.play();
-
-    // Crear más partículas con un radio más grande
-    for (let i = 0; i < 50; i++) { // Aumentamos la cantidad de partículas
-        crearParticula(event.clientX, event.clientY);
-    }
-});
-
-function crearParticula(x, y) {
-    let particula = document.createElement('span');
-    document.body.appendChild(particula);
-    
-    // Estilos en JavaScript para la partícula
-    let size = Math.random() * 15 + 4; // Tamaño aleatorio entre 4px y 12px
-    Object.assign(particula.style, {
-        position: "absolute",
-        width: size + 'px',
-        height: size + 'px',
-        background: `#285ae694`, 
-        borderRadius: "100%",
-        left: x + 'px',
-        top: y + 'px',
-        transition: "transform 1s ease-out, opacity 1s",
-    });
-
-    // Movimiento aleatorio más grande
-    let angle = Math.random() * 2 * Math.PI; // Dirección aleatoria
-    let speed = Math.random() * 100 + 50; // Más dispersión
-    let translateX = Math.cos(angle) * speed;
-    let translateY = Math.sin(angle) * speed;
-
-    setTimeout(() => {
-        particula.style.transform = `translate(${translateX}px, ${translateY}px) scale(0)`;
-        particula.style.opacity = "0";
-    }, 10);
-
-    // Eliminar la partícula después de la animación
-    setTimeout(() => {
-        particula.remove();
-    }, 1000);
-}
-
-
-
 
 
 
